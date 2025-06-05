a492955446f5bc366854680254dd8679c19b50e9
Julipv <vasquezjuli310@gmail.com>
2025-06-05 15:23:56 -0500

    creacion de proyecto par impar

---
+++ NumeroParImpar/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+
+do
+{
+
+    Console.Write("ingrese un numero");
+    var numero = int.Parse(Console.ReadLine());
+
+    if (numero % 2 == 0)
+    {
+        Console.WriteLine("tu numero es par");
+    }
+    else
+    {
+        Console.WriteLine("tu numero es impar");
+    }
+
+
+
+
+
+} while (true);
