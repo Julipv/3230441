7f332264f3d93367097b859d6040a1c71329ff00
Julipv <vasquezjuli310@gmail.com>
2025-06-12 16:05:07 -0500

    trabajo

---
+++ ConceptosProgramacion/Ejercicio/Program.cs
@@ -0,0 +1,51 @@
+﻿
+do
+{
+    Console.WriteLine("INGRESE PRIMER NUMERO ENTERO");
+  var N1= int.Parse(Console.ReadLine());
+    Console.WriteLine("INGRESE EL SEGUNDO NUMERO ENTERO");
+    var N2 = int.Parse(Console.ReadLine());
+    Console.WriteLine("INGRESE EL TERCER NUMERO ENTERO");
+    var N3 = int.Parse(Console.ReadLine());
+
+    if (N1 > N2 & N2 > N3)
+
+    { 
+        Console.WriteLine($"EL NUMERO MAYOR ES {N1}");
+        Console.WriteLine($"EL NUMERO  DEL MEDIO ES {N2}");
+        Console.WriteLine($"EL NUMERO MENOR ES {N3}");
+    }
+
+    else if (N1 > N3 & N3 > N2)
+    {
+        Console.WriteLine($"EL NUMERO MAYOR ES {N1}");
+        Console.WriteLine($"EL NUMERO DEL MEDIO ES {N3}");
+        Console.WriteLine($"EL NUMERO MENOR ES {N1}");
+    }
+
+    else if (N2 > N1 & N1 > N3)
+    {
+        Console.WriteLine($"EL NUMERO MAYOR ES {N2}");
+        Console.WriteLine($"EL NUMERO DEL MEDIO ES {N1}");
+        Console.WriteLine($"EL NUMERO MENOR ES {N3}");
+    }
+    else if (N2 > N3 & N3 > N1)
+    {
+        Console.WriteLine($"EL NUMERO MAYOR ES {N2}");
+        Console.WriteLine($"EL NUMERO DEL MEDIO ES {N3}");
+        Console.WriteLine($"EL NUMERO MENOR ES {N1}");
+    }
+    else if (N3 > N1 & N1 > N2)
+    {
+        Console.WriteLine($"EL NUMERO MAYOR ES {N3}");
+        Console.WriteLine($"EL NUMERO DEL MEDIO ES {N1}");
+        Console.WriteLine($"EL NUMERO MENOR ES {N2}");
+    }
+    else
+    {
+        Console.WriteLine($"EL NUMERO MAYOR ES {N3}");
+        Console.WriteLine($"EL NUMERO DEL MEDIO ES {N2}");
+        Console.WriteLine($"EL NUMERO MENOR ES {N1}");
+    }
+}
+    while (true) ;
---
+++ ConceptosProgramacion/Numeros/Program.cs
@@ -0,0 +1,36 @@
+﻿do
+{
+    Console.WriteLine("INGRESE PRIMER NUMERO ENTERO");
+    var N1 = (Console.ReadLine());
+
+    if (int.TryParse(N1, out int N1int))
+    {
+
+        Console.WriteLine("INGRESE EL SEGUNDO NUMERO ENTERO");
+        var N2 = (Console.ReadLine());
+
+        if (int.TryParse(N2, out int N2int)) ;
+        {
+            Console.WriteLine("INGRESE EL SEGUNDO NUMERO ENTERO");
+
+        }
+        if (N1int % N2int == 0)
+        {
+            Console.WriteLine($"EL NUMERO {N1} ES MULTIPLO DE {N2}");
+        }
+
+        else
+        {
+
+            Console.WriteLine($"EL NUMERO {N1} NO ES MULTIPLO DE {N2}");
+        }
+       
+    }
+
+    else
+    {
+        Console.WriteLine("DEBES INGRESAR UN NUMERO ENTERO");
+  
+    }
+}
+while (true);
